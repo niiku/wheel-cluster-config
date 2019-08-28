c0b7174d3909b260c7bb0b6b197fffcdc9a3b811
Philips, Nikolas <nikolas.philips@baloise.ch>
2019-08-28 10:47:06 +0200

    Remove entrypoint.py hack / Update version

--- confluence/hack/patch.sh
+++
@@ -1,1 +0,0 @@
-oc patch deployment confluence-server -p '{"spec":{"template":{"spec":{"containers":[{"name":"confluence","volumeMounts":[{"mountPath":"/entrypoint.py","name":"entrypoint","subPath":"entrypoint.py"}]}]}}}}'
